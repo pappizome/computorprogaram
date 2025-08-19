9e2f3ab995a6dbd734d096bcfc83125bfa0253c7
pappizome <lijames329@gmail.com>
2025-08-19 14:54:23 -0700

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,5 @@
+import random 
+lakers = (" lebron james " , " magic johnson " ," bronny james " , " luka donicic ")
+random.lakers = random.choice(lakers)
+            print(f"{r:2} " + ' '.join(row))``
+print (random.choice(lakers))
